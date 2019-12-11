a56ccd754176d02d65694eacfa2e1e7f72ac69fb
Gustavo Conte <58783067+gstvcnt@users.noreply.github.com>
2019-12-11 12:46:01 -0400

    Comentários Adicionados

--- Quadrado da Diferença.py
+++ Quadrado da Diferença.py
@@ -1,11 +1,11 @@
-import os
+import os #Importa módulos para integração com o sistema
 print('QUADRADO DA DIFERENÇA (X - A)²')
 a = int(input('\nDigite o valor de A\n'))
 b = int(input('\nDigite o valor de B\n'))
-inicio = a * a
+inicio = a * a #Posteriormente encontrar outra forma de realizar potenciação
 meio = 2 * a * b
 fim = b * b
-core = inicio - meio + fim
-print('O resultado é ',core)
+core = inicio - meio + fim #Lógica principal da operação
+print('O resultado é ',core) #Imprime resultado
 input('\nPressione qualquer tecla para sair')
-os.system('cls')
+os.system('cls') #Limpa terminal
