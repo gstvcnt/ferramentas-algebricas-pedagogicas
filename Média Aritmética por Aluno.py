c5750f3bcbe631b4bf334b2ec325c76d926fa64b
Gustavo Conte <58783067+gstvcnt@users.noreply.github.com>
2019-12-12 03:21:51 -0400

    Erros de grafia corrigidos

--- Média Aritmética por Aluno.py
+++ Média Aritmética por Aluno.py
@@ -2,9 +2,9 @@ def main(): #Define Função Principal do programa
     aluno = input('Nome do Aluno\n\n') #Obtém notas do aluno em ponto flutuante
 
     primeiro = float(input('\nNota no 1° Bimestre\n'))
-    segundo = float(input('\nNota do 2° Biimestre\n'))
-    terceiro = float(input('\nNota do 3° Bimestre\n'))
-    quarto = float(input('\nNota do 4° Bimestre\n'))
+    segundo = float(input('\nNota no 2° Bimestre\n'))
+    terceiro = float(input('\nNota no 3° Bimestre\n'))
+    quarto = float(input('\nNota no 4° Bimestre\n'))
 
     soma = primeiro + segundo + terceiro + quarto
     media_aluno = soma / 4 #Realiza média aritmética de quatro bimestres
