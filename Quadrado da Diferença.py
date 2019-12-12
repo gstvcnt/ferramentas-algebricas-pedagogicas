d58284ac7bbc1fd75d910be9a209d63be69e30f1
Gustavo Conte <58783067+gstvcnt@users.noreply.github.com>
2019-12-12 11:24:57 -0400

    Melhorias no código
    Estrutura de repetição
    Melhoria na interação-usuário
    
    *Lógica inalterada*

--- Quadrado da Diferença.py
+++ Quadrado da Diferença.py
@@ -1,11 +1,22 @@
-import os #Importa módulos para integração com o sistema
-print('QUADRADO DA DIFERENÇA (X - A)²')
-a = int(input('\nDigite o valor de A\n'))
-b = int(input('\nDigite o valor de B\n'))
-inicio = a * a #Posteriormente encontrar outra forma de realizar potenciação
-meio = 2 * a * b
-fim = b * b
-core = inicio - meio + fim #Lógica principal da operação
-print('O resultado é ',core) #Imprime resultado
-input('\nPressione qualquer tecla para sair')
-os.system('cls' if os.name == 'nt' else 'clear') #Limpa terminal Multiplataforma
+stop = False
+while not stop:
+    
+    import os #Módulo sistema
+    a = int(input('Digite o valor de A\n'))
+    b = int(input('\nDigite o valor de B\n'))
+    inicio = a * a
+    meio = 2 * a * b
+    fim = b * b
+    core = inicio - meio + fim #Lógica central do produto notável
+    os.system('cls' if os.name =='nt' else 'clear')
+    
+    print('RESOLUÇÃO\n')
+    print('Valor de A =' ,a)
+    print('Valor de B =' ,b, '\n')
+    print(a,'²' '+' ' 2' ' x' ,a, 'x' ,b, '+' ,b, '²' '=' ,core)
+    print('\nO quadrado da diferença entre' ,a, 'e' ,b, 'é' ,core)
+    input('\nPressione ENTER para realizar o cálculo novamente')
+    os.system('cls' if os.name =='nt' else 'clear')#Limpa terminal para realização de outro cálculo
+
+    if input == "":
+        stop = True
