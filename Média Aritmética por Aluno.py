96ba787ef37ff5dbb049bf02f8beeabf331505b0
Gustavo Conte <58783067+gstvcnt@users.noreply.github.com>
2019-12-12 04:40:06 -0400

    Funcionalidade de repetição do código
    O código é repetido até que o usuário encerre o processo, evitando que ele tenha que executar novamente o programa.

--- Média Aritmética por Aluno.py
+++ Média Aritmética por Aluno.py
@@ -1,5 +1,7 @@
-def main(): #Define Função Principal do programa
-    aluno = input('Nome do Aluno\n\n') #Obtém notas do aluno em ponto flutuante
+stop = False
+while not stop:
+    import os #Importa Módulo para integração com o Sistema Operacional
+    aluno = input('Nome do Aluno\n\n') 
 
     primeiro = float(input('\nNota no 1° Bimestre\n'))
     segundo = float(input('\nNota no 2° Bimestre\n'))
@@ -10,6 +12,8 @@ def main(): #Define Função Principal do programa
     media_aluno = soma / 4 #Realiza média aritmética de quatro bimestres
     round(media_aluno, 2) #Arredonda para duas casas decimais
     print('\nO Aluno' ,aluno, 'alcançou média' ,media_aluno, '\n')
-    input('\nPressione uma tecla para realizar o cálculo novamente\n')
-    os.system('cls' if os.name =='nt' else 'clear') #Limpa terminal, cls em Windows NT caso contrário clear em UNIX Like
-main()
+    input('\nPressione ENTER para realizar o cálculo novamente\n')
+    os.system('cls' if os.name =='nt' else 'clear') #Limpa terminal, cls em Windows NT caso contrário "clear" em UNIX Like
+    
+    if input == "":
+        stop = True
