cbcb6ba84a59a66859db7395a0881e0806dbc8b2
Gustavo Conte <58783067+gstvcnt@users.noreply.github.com>
2020-02-27 16:34:23 -0400

    REGIONS: Alterações para melhor leitura
    Separação das regions no código

--- Quadrado da Diferença.py
+++ Quadrado da Diferença.py
@@ -1,12 +1,15 @@
+import os #Módulo sistema
+    
 stop = False
 while not stop:
     
-    import os #Módulo sistema
     a = int(input('Digite o valor de A\n'))
     b = int(input('\nDigite o valor de B\n'))
+    
     inicio = a * a
     meio = 2 * a * b
     fim = b * b
+    
     core = inicio - meio + fim #Lógica central do produto notável
     os.system('cls' if os.name =='nt' else 'clear')
     
@@ -15,6 +18,7 @@ while not stop:
     print('Valor de B =' ,b, '\n')
     print(a,'²' '+' ' 2' ' x' ,a, 'x' ,b, '+' ,b, '²' '=' ,core)
     print('\nO quadrado da diferença entre' ,a, 'e' ,b, 'é' ,core)
+    
     input('\nPressione ENTER para realizar o cálculo novamente')
     os.system('cls' if os.name =='nt' else 'clear')#Limpa terminal para realização de outro cálculo
 
